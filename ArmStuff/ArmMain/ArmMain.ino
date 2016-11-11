edc2a3f1dddf21123f1f51a3a6fe595831790515
Gabe Casciano <gabecasciano@gmail.com>
2016-11-10 22:39:01 -0500

    Main and constants files

---
+++ ArmStuff/ArmMain/ArmMain.ino
@@ -0,0 +1,13 @@
+//This is the main arm file
+#include "Constants.h"
+
+Constants constants = new Constants();
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
