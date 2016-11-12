1ff08e46649fe5d057f40dc3a71d457136216a47
Gabe Casciano <gabecasciano@gmail.com>
2016-11-11 22:36:29 -0500

    New stuff

--- ArmStuff/ArmMain/ArmMain.ino
+++ ArmStuff/ArmMain/ArmMain.ino
@@ -1,13 +1,23 @@
 //This is the main arm file
 #include "Constants.h"
+#include "Motor.h"
+#include "Potentiometer.h"
+#include "Encoder.h"
+#include "Wire.h"
 
 Constants constants = new Constants();
+Motor motors[] = new Motor[];
+Potentiometer pots = new Potentiometer[];
+
+int potValues[] = new int[4];//contains the current position of the pots
+int encoderValues[] = new int[2];//contains the current count values for the encoders channels
+int motorOutputs[][] = new int[5][2];//contains the motor outputs and their directions
+
 void setup() {
-  // put your setup code here, to run once:
-
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
-
 }
+void recieveData(){
+	
+}
+
