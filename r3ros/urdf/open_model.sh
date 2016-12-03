0bea0c404519f9d9b8630f7b0a0e654a23164fb0
danielsnider <danielsnider12@gmail.com>
2016-12-03 11:31:15 -0800

    Improved robot model
    Added wheels, a launch file, and move its file path

--- r3ros/urdf/open_model.sh
+++ r3ros/urdf/open_model.sh
@@ -2,4 +2,4 @@
 
 model_file=${1:-robot.urdf} # set default file to open if one is not specified on command line
 
-roslaunch urdf_tutorial display.launch  model:=$model_file
+roslaunch urdf_tutorial display.launch  model:=$model_file gui:=True
