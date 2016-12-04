13b2e7f1dbbeb8a75a8c99f8bbc9088ae9cf3a6c
danielsnider <danielsnider12@gmail.com>
2016-12-04 21:59:11 +0000

    Added and updated video stream code

---
+++ dan/stream_video/stream_usb_camera.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import time
+import rospy
+from sensor_msgs.msg import Image
+from sensor_msgs.msg import CompressedImage
+
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
+
+def camera():
+  rospy.loginfo("starting...")
+  rospy.init_node('VideoPublisher', anonymous=True)
+  camera = rospy.Publisher('camera', Image)
+  camera_compressed = rospy.Publisher('camera/compressed', CompressedImage)
+  rate = rospy.Rate(10) # 24hz
+  cap = cv2.VideoCapture(1) # open first camera device
+
+  if not cap.isOpened():
+    rospy.loginfo("capture is not open :(")
+
+  while not rospy.is_shutdown() and cap.isOpened():
+    rospy.loginfo("sending frame...")
+    ret, frame = cap.read()
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+      rospy.logwarn("breaking...")
+      break
+    if not ret:
+      rospy.logwarn("something wrong opening frame")
+      break
+
+    #msg_frame = CvBridge().cv2_to_imgmsg(frame, encoding='bgr8')
+    msg_frame_compressed = CvBridge().cv2_to_compressed_imgmsg(frame)
+    #camera.publish(msg_frame)
+    camera_compressed.publish(msg_frame_compressed)
+    rate.sleep()
+
+  cap.release()
+
+if __name__ == '__main__':
+  try:
+    camera()
+  except rospy.ROSInterruptException:
+    pass
