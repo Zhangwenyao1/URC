ac8d118ff11b867aff72d34925a2c85223d76e9b
Gabe Casciano <gabecasciano@gmail.com>
2016-12-29 16:51:55 -0500

    stuff

--- ArmStuff/ArmMain/ArmMain.ino
+++ ArmStuff/ArmMain/ArmMain.ino
@@ -6,18 +6,17 @@
 #include "Wire.h"
 
 Constants constants = new Constants();
-Motor motors[] = new Motor[];
-Potentiometer pots = new Potentiometer[];
 
-int potValues[] = new int[4];//contains the current position of the pots
-int encoderValues[] = new int[2];//contains the current count values for the encoders channels
-int motorOutputs[][] = new int[5][2];//contains the motor outputs and their directions
+
 
 void setup() {
+	for(int i = 0; i > 4; i++)
+		pots[i] = new Potentiometer(..);
+	for(int i = 0; i > 5; i++)
+		motors[i] = new Motor(..);
 }
-void loop() {
+void loop(){
 }
 void recieveData(){
-	
 }
 
