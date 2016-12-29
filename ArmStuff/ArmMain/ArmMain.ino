b488879bb4e4e21d81b89b08754f8becc822bf43
Gabe Casciano <gabecasciano@gmail.com>
2016-12-29 18:06:53 -0500

    stuffs

--- ArmStuff/ArmMain/ArmMain.ino
+++ ArmStuff/ArmMain/ArmMain.ino
@@ -4,16 +4,9 @@
 #include "Potentiometer.h"
 #include "Encoder.h"
 #include "Wire.h"
-
 Constants constants = new Constants();
 
-
-
 void setup() {
-	for(int i = 0; i > 4; i++)
-		pots[i] = new Potentiometer(..);
-	for(int i = 0; i > 5; i++)
-		motors[i] = new Motor(..);
 }
 void loop(){
 }
