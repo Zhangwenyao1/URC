cba08f684bd27dde1f9b931f913d71ec7e1809d0
Gabe Casciano <gabecasciano@gmail.com>
2017-01-02 15:46:45 -0500

    More functionality

--- ArmStuff/ArmMain/ArmMain.ino
+++ ArmStuff/ArmMain/ArmMain.ino
@@ -1,5 +1,8 @@
 //This is the main arm file
 #include "Motor.h";
+#include "Encoder.h";
+#include "Potentiometer.h";
+
 //motor pins
 #define M0 0
 #define M1 1
@@ -7,20 +10,23 @@
 #define M3 3
 #define M4 4
 #define M5 5
-//direction pins
-
+//motor array constructor
+Motor motors[1] = {
+  Motor(M0,M1)
+};
 //pot pins 
 #define pot0 A0
 #define pot1 A1
 #define pot2 A2
 #define pot3 A3
-//encoder pins
-#define enca0 6
-#define encb0 7
-
-Motor motors[1] = {
-  Motor(M0,M1)
+//potentiometer array constructor
+Potentiometer pots[4]={
+  Potentiometer(pot0),
+  Potentiometer(pot1),
+  Potentiometer(pot2),
+  Potentiometer(pot3)  
 };
+
 void setup() {
 }
 void loop(){
