60700c2b22f667ba4878e571c0caae157e843630
Gabe Casciano <gabecasciano@gmail.com>
2017-01-02 15:26:59 -0500

    Cleaning up

--- ArmStuff/ArmMain/ArmMain.ino
+++ ArmStuff/ArmMain/ArmMain.ino
@@ -1,15 +1,25 @@
 //This is the main arm file
-#include "Constants.h"
-#include "Motor.h"
-#include "Potentiometer.h"
-#include "Encoder.h"
-#include "Wire.h"
-Constants constants = new Constants();
+#include "Motor.h";
+//motor pins
+#define M0 0
+#define M1 1
+#define M2 2
+#define M3 3
+#define M4 4
+#define M5 5
+//pot pins 
+#define pot0 A0
+#define pot1 A1
+#define pot2 A2
+#define pot3 A3
+//encoder pins
+#define enca0 6
+#define encb0 7
 
+Motor motors[1] = {
+  Motor(M0,M1)
+};
 void setup() {
 }
 void loop(){
 }
-void recieveData(){
-}
-
