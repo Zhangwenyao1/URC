c9c7d007f734433ed841fd3378fc415eb4131fa7
Gabe Casciano <gabecasciano@gmail.com>
2017-01-02 15:28:49 -0500

    Rebuilding Classes

---
+++ ArmStuff/ArmMain/Motor.cpp
@@ -0,0 +1,18 @@
+#include "Arduino.h"
+#include "Motor.h"
+
+Motor::Motor(int _pin, int _dirPin){
+  pin = _pin;
+  dirPin = _dirPin;
+}
+void Motor::doMotors(double input){
+  bool direction;
+  int speed;
+  if(input < 0)
+    direction = false;
+  else 
+    direction = true;
+  
+  digitalWrite(dirPin,direction);
+  analogWrite(pin, map(0,1,0,255,abs(input)));
+}
---
+++ ArmStuff/ArmMain/Motor.h
@@ -0,0 +1,12 @@
+#ifndef Motor_h
+#define Motor_h
+#include "Arduino.h"
+class Motor{
+  public:
+    Motor(int _pin, int _dirPin);
+    void doMotors(double input);
+  private:
+    int pin;
+    int dirPin;
+};
+#endif
