97f6618480b0976f5deda8166450d118619121b0
Gabe Casciano <gabecasciano@gmail.com>
2017-02-02 13:44:59 -0500

    added classes to driveduino

---
+++ ArmStuff/Driveduino/Encoder.cpp
@@ -0,0 +1,25 @@
+#include "Arduino.h"
+#include "Encoder.h"
+#include "ros.h";
+#include <std_msgs/UInt16.h>
+#include <std_msgs/Float32.h>
+
+Encoder::Encoder(){}
+Encoder::Encoder(int A, int B){
+  pinA = A;
+  pinB = B;
+  countValueA = 0;
+  countValueB = 0;
+  pinMode(pinA, INPUT);
+  pinMode(pinB, INPUT);
+}
+uint16_t Encoder::getEncoderValues(){
+	uint16_t value = (countValueA+countValueB)/2;
+	return (value > 0) ? (value):(1);
+}
+void Encoder::doEncoderA(){
+	countValueA++;
+}
+void Encoder::doEncoderB(){
+	countValueB++;
+}
