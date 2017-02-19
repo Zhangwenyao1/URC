c76c037f4e3d12ea55271be94a0bfcc9a4b61918
mincrmatt12 <matthewmirvish@hotmail.com>
2017-02-18 20:48:57 -0500

    Few things:
    Finish protocol docs for the arm code
    Correct weight values in the urdf
    Almost finish the actual actionserver code in python

--- rosws/src/rover_arm/README.md
+++ rosws/src/rover_arm/README.md
@@ -1,25 +1,64 @@
 # Protocol Definition
 ## General
 
-Baudrate: 9600
+* Baudrate: 9600
+* Sent over serial
 
-## TX1 -> Arduino
+## Message format
+
+The data can be sent from
+
+### Header
+
+---
+byte|null|null|...
 
 ---
 
-byte|null|null|...
+The first byte (unsigned) is the command type. If it is not in the table below, discard the message.
 
-The first byte (unsigned) is the command type. If it is not in [1, 2], discard the message.
+| Value | Meaning when sent from the TX1 | Meaning when sent from the Arduino |
+| --- | :---: | :---: |
+| 1 | New setpoint list, arm data | New arm joint data |
+| 2 | Abort everything | Request to stop |
+| 3 | New setpoint list, gripper data | New gripper joint data |
+| 4 | Path has ended | unused
 
-| Value | Meaning |
-| --- | --- |
-| 1 | New setpoint list |
-| 2 | Abort everything |
+### Setpoint/joint data format
 
-When aborting, there is no more data.
+After the message header, either 2 or 4 floats (IEE 754) are sent.
 
-### Setpoint list format
+When the arm data is sent, 4 floats are sent representing these joints (in order):
 
-After the message "header" (byte null null), 6 floats (IEE 754) are sent.
+* shoulder, connecting the spinning thing to the first arm segment (pitch)
+* elbow, connecting the first arm segment to the second (pitch)
+* lower_elbow, (I didn't name these), connecting the second arm segment to the piece that holds the gripper base (pitch) 
+* hip, the spinning thing at the base (yaw)
 
+Whilst when the gripper is sent, these 2 are sent:
+
+* grip_twist, the part that twists the gripper (roll)
+* grip, the part that actually moves, (prismatic)
+
+Rotations are in radians, and the gripper is in metres.
+
+The floats are packed like this:
+
+---
 float|null|float|null ... float|null
+
+---
+
+### Abort/path end format
+Abort contains nothing after the header.
+
+## Behavior
+
+When the TX1 receives a new path, it sends a type 1/3 message corresponding to the type of movement.
+It then waits for a similarly typed message from the arduino. It then checks if there are anymore messages to send.
+If there are, then repeat, else send a type 4.
+
+If at anytime anything is wrong, the arduino should send a type 2 message, and if tolerances are violated/a stop request is sent to the TX1,
+it will send a type 2 message. When the arduino sends or receives a type 2 message, it should stop moving immediately and act as if the path has finished.
+If the TX1 sends or receives one, it will set the result to `ABORTED` and stop sending messages until it gets a new path.
+ 
--- rosws/src/rover_arm/src/arm_serial.py
+++ rosws/src/rover_arm/src/arm_serial.py
@@ -8,18 +8,22 @@ import time
 import struct
 import serial
 
-JOINT_ORDERINGS = [
-    "elbow_lower",
-    "elbow_middle",
-    "elbow_upper",
-    "base_twist",
-    "grip",
-    "grip_twist"
-]
+JOINT_ORDERINGS = {
+    4: [
+        "shoulder",
+        "elbow",
+        "lower_elbow",
+        "hip"
+    ],
+    2: [
+        "grip_twist",
+        "grip"
+    ]
+}
 
 
 class SerialCommunicator(threading.Thread):
-    def __init__(self, prt, new_waypoint_cb, joint_names):
+    def __init__(self, prt, new_waypoint_cb,):
         """
 
         :type prt: serial.Serial
@@ -29,17 +33,16 @@ class SerialCommunicator(threading.Thread):
         self.serial_p = prt
         self.done_waypoint_cb = new_waypoint_cb
         self.waypoint_queue = Queue()
-        self.joint_names = joint_names
 
     def _send_waypoint_message(self, waypoint, joint_names):
         # type: (trajectory_msgs.msg.JointTrajectory) -> None
-        n_joints = len(JOINT_ORDERINGS)
+        n_joints = len(joint_names)
         joint_positions = []
-        for pub_joint in JOINT_ORDERINGS:
+        for pub_joint in JOINT_ORDERINGS[n_joints]:
             ind = joint_names.index(pub_joint)
             joint_positions.append(waypoint.positions[ind])
         fmtstr = "<bxx" + "fx" * n_joints
-        packed = struct.pack(fmtstr, 1, *joint_positions)
+        packed = struct.pack(fmtstr, 1 if n_joints == 4 else 3, *joint_positions)
         self.serial_p.write(packed)
 
     def abort(self):
@@ -47,16 +50,22 @@ class SerialCommunicator(threading.Thread):
 
     def _read_status(self):
         n_joints = len(JOINT_ORDERINGS)
-        fmtstr = "<" + "fx" * n_joints
-        data = self.serial_p.read(struct.calcsize(fmtstr))
+        fmtstr = "<bxx"
+        typ = self.serial_p.read(struct.calcsize(fmtstr))
+        if typ == 2:
+            self.abort()
+            return
+        else:
+            fmtstr = "<" + "fx" * (4 if typ == 1 else 2)
         data = struct.unpack(fmtstr, data)
         self.done_waypoint_cb(False, data)
 
     def run(self):
         while True:
             next_waypoint = self.waypoint_queue.get()
-            self._send_waypoint_message(next_waypoint, self.joint_names)
+            self._send_waypoint_message(next_waypoint[1], next_waypoint[0])
             got_response = False
+            l = next_waypoint[0]
             while not got_response:
                 if self.serial_p.in_waiting > 0:
                     time.sleep(0.5)
@@ -66,8 +75,8 @@ class SerialCommunicator(threading.Thread):
                     self.waypoint_queue = Queue()
                     self._send_abort()
                     got_response = True
-                    self.is_aborting = True
-                    self.done_waypoint_cb(True, [])
+                    self.is_aborting = False
+                    self.done_waypoint_cb(True, l)
 
     def _send_abort(self):
         self.serial_p.write('\x02\x00\x00')
@@ -75,19 +84,31 @@ class SerialCommunicator(threading.Thread):
 
 class ArmActionServer:
     def __init__(self, serial_):
-        self.as_ = actionlib.SimpleActionServer("arm/follow_joint_trajectory",
-                                                control_msgs.msg.FollowJointTrajectoryAction,
-                                                auto_start=False)
+        self.as_arm = actionlib.SimpleActionServer("arm_controller/follow_joint_trajectory",
+                                                   control_msgs.msg.FollowJointTrajectoryAction,
+                                                   auto_start=False)
+        self.as_gripper = actionlib.SimpleActionServer("grip_controller/follow_joint_trajectory",
+                                                       control_msgs.msg.FollowJointTrajectoryAction,
+                                                       auto_start=False)
         self._feedback_message = control_msgs.msg.FollowJointTrajectoryActionFeedback()
         self._result_message = control_msgs.msg.FollowJointTrajectoryActionResult()
-        self.serial_thread = SerialCommunicator(serial_, self.next_waypoint, JOINT_ORDERINGS)
+        self.serial_thread = SerialCommunicator(serial_, self._next_waypoint)
 
-    def next_waypoint(self):
+        self.as_arm.register_goal_callback(lambda: self.goal_callback(4))
+        self.as_arm.register_preempt_callback(lambda: self.preempt_callback(4))
+        self.as_gripper.register_goal_callback(lambda: self.goal_callback(2))
+        self.as_gripper.register_preempt_callback(lambda: self.preempt_callback(2))
+
+    def _get_as(self, n):
+        return self.as_arm if n == 4 else self.as_gripper
+
+    def _next_waypoint(self, stopped, joints):
+        if stopped:
+            self._result_message.
+            self._get_as(len(joints)).set_aborted()
+
+    def goal_callback(self, as_):
         pass
 
-    def goal_callback(self):
+    def preempt_callback(self, as_):
         pass
-
-    def preempt_callback(self):
-        pass
-
