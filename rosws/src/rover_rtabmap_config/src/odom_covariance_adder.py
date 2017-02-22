77c3d8e562448584f6aec45b67c4cb9af588dd82
mincrmatt12 <matthewmirvish@hotmail.com>
2017-02-21 20:19:10 -0500

    PAAARAAAMS!

--- rosws/src/rover_rtabmap_config/src/odom_covariance_adder.py
+++ rosws/src/rover_rtabmap_config/src/odom_covariance_adder.py
@@ -8,19 +8,24 @@ odom_out = Odometry()
 
 def callback(data):
     odom_out.pose = data.pose
-    odom_out.pose.covariance[0] = -1
-    odom_out.pose.covariance[7] = -1
-    odom_out.pose.covariance[14] = -1
-    odom_out.pose.covariance[21] = -1
-    odom_out.pose.covariance[28] = -1
-    odom_out.pose.covariance[35] = -1
+    c_hack = list(odom_out.pose.covariance)
+    c_hack[0] = -1
+    c_hack[7] = -1
+    c_hack[14] = -1
+    c_hack[21] = -1
+    c_hack[28] = -1
+    c_hack[35] = -1
+    odom_out.pose.covariance = tuple(c_hack)
+    
     odom_out.twist = data.twist
-    odom_out.twist.covariance[0] = -1
-    odom_out.twist.covariance[7] = -1
-    odom_out.twist.covariance[14] = -1
-    odom_out.twist.covariance[21] = -1
-    odom_out.twist.covariance[28] = -1
-    odom_out.twist.covariance[35] = -1
+    c_hack = list(odom_out.twist.covariance)
+    c_hack[0] = -1
+    c_hack[7] = -1
+    c_hack[14] = -1
+    c_hack[21] = -1
+    c_hack[28] = -1
+    c_hack[35] = -1
+    odom_out.twist.covariance = tuple(c_hack)
     pub.publish(odom_out)
 
 pub = rospy.Publisher("/zed/odom_fixed", Odometry, queue_size=10)
