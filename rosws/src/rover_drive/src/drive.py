1c24c9d8278baedc2ea19a1945a7cd5eedd45aff
danielsnider <danielsnider12@gmail.com>
2017-03-30 22:59:36 +0000

    Config updates done while testing autonomous

--- rosws/src/rover_drive/src/drive.py
+++ rosws/src/rover_drive/src/drive.py
@@ -7,10 +7,12 @@ import struct
 
 
 def on_new_twist(data):
-    dat = struct.pack("<ff", data.linear.x, data.angular.z)
+    dat = struct.pack("<ff", data.linear.x/1.2, (-data.angular.z)/2)
     theSerial.write(dat)
 
 theSerial = serial.Serial()
+theSerial.port = "/dev/ttyACM0"
+theSerial.open()
 
 rospy.init_node("twist_sender")
 subscriber = rospy.Subscriber("/cmd_vel", geometry_msgs.msg.Twist, on_new_twist, queue_size=10)
