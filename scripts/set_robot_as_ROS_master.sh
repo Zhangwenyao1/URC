c3eafd40b09a224c6aae62edc44d4cee604dcd97
danielsnider <danielsnider12@gmail.com>
2017-04-04 06:03:40 -0700

    Improved set_robot_as_ROS_master.sh with sanity check and only set one ROS_IP
    Usage: $ source set_robot_as_ROS_master.sh

--- scripts/set_robot_as_ROS_master.sh
+++ scripts/set_robot_as_ROS_master.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-export ROS_IP=`hostname -I`
+hostname -I | grep -q 192.168.137 
+exit_status=$?
+if [ $exit_status -eq 1 ]; then
+    echo "ERROR: you do not have an IP on the robot network (192.168.137.x). Please set one. Aborting..."
+    return 1
+fi
+
 export ROS_MASTER_URI=http://192.168.137.212:11311/
+export ROS_IP=`hostname -I|cut -f1 -d' '`
