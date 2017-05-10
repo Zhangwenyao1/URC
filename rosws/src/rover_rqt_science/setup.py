47be8f3b4cb6c4795af7aa3db15e34ca318148f0
mincrmatt12 <matthewmirvish@hotmail.com>
2017-05-09 21:55:38 -0400

    start panorama gui, start excel exporter for science

--- rosws/src/rover_rqt_science/setup.py
+++ rosws/src/rover_rqt_science/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-    packages=['rqt_rover_science'],
+    packages=['rqt_rover_science', 'rqt_rover_panorama'],
     package_dir={'': 'src'},
 )
 
