56f88125679532a80daaad462bdaa17d1014e4be
mincrmatt12 <matthewmirvish@hotmail.com>
2017-05-11 16:22:37 -0400

    gitgud

--- rosws/src/rover_drive/src/drive.py
+++ rosws/src/rover_drive/src/drive.py
@@ -4,10 +4,29 @@ import rospy
 import geometry_msgs.msg
 import serial
 import struct
+import rover_drive.msg
+
+cmode = False
+
+
+def transmit_if(t):
+    global cmode
+    if cmode == t:
+        return
+    else:
+        theSerial.write("\x00" if not t else "\x01")
+        cmode = t
 
 
 def on_new_twist(data):
-    dat = struct.pack("<ff", data.linear.x/5.7, (-data.angular.z)/5.777)
+    transmit_if(False)
+    dat = "\x02" + struct.pack("<ff", data.linear.x/5.7, (-data.angular.z)/5.777)
+    theSerial.write(dat)
+
+
+def on_new_tank(data):
+    transmit_if(True)
+    dat = "\x02" + struct.pack("<ff", data.left, data.right)
     theSerial.write(dat)
 
 theSerial = serial.Serial()
@@ -15,5 +34,6 @@ theSerial.port = "/dev/ttyACM0"
 theSerial.open()
 
 rospy.init_node("twist_sender")
-subscriber = rospy.Subscriber("/cmd_vel", geometry_msgs.msg.Twist, on_new_twist, queue_size=10)
+subscriber = rospy.Subscriber("/cmd_vel", geometry_msgs.msg.Twist, on_new_twist, queue_size=15)
+subscriver = rospy.Subscriber("/cmd_vel_tank", rover_drive.msg.Tank, on_new_tank, queue_size=15)
 rospy.spin()
--- rosws/src/rover_teleoperation/src/joy_teleoperation_node.py
+++ rosws/src/rover_teleoperation/src/joy_teleoperation_node.py
@@ -2,19 +2,33 @@
 import rospy
 from sensor_msgs.msg import Joy
 from geometry_msgs.msg import Twist
+import rover_drive.msg
 
+mode = False
 
 def on_joy_data(data):
+    global mode
     """
 
     :type data: Joy
     """
-    msg = Twist()
-    msg.linear.x = data.axes[1]
-    msg.angular.z = data.axes[0]
-    pub.publish(msg)
+    if data.buttons[1]:
+        mode = not mode
+
+    if not mode:
+        msg = Twist()
+        msg.linear.x = data.axes[1]
+        msg.angular.z = data.axes[0]
+        pub.publish(msg)
+    else:
+        msg = rover_drive.msg.Tank()
+        msg.left = data.axes[1]
+        msg.right = data.axes[4]
+        pub2.publish(msg)
+
 
 rospy.init_node("joystick_teleoperation")
 sub = rospy.Subscriber("joy", Joy, on_joy_data)
 pub = rospy.Publisher("cmd_vel_mux/teleoperation", Twist, queue_size=1)
+pub2 = rospy.Publisher("cmd_vel_tank", rover_drive.msg.Tank, queue_size=1)
 rospy.spin()
