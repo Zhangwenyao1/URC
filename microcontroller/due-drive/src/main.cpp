7a62ba6c054e3fa75c813619b035586b14c09a0e
danielsnider <danielsnider12@gmail.com>
2017-05-21 13:54:21 -0400

    drive fix!

--- microcontroller/due-drive/src/main.cpp
+++ microcontroller/due-drive/src/main.cpp
@@ -9,7 +9,7 @@
 #define pinR3 7
 #define sparkMax 2000
 #define sparkMin 1000
-#define pinZEDServo 2
+#define pinZEDServo 5
 
 Servo ZEDServo;
 Servo leftOne;
@@ -50,31 +50,31 @@ void loop() {
    if (SerialUSB.available() >= sizeof(uint8_t)) {
        delayMicroseconds(10);
        uint8_t cmd = (uint8_t) SerialUSB.read();
-       SerialUSB.print("GOT");
-       SerialUSB.println(cmd);
+       // SerialUSB.print("GOT");
+       // SerialUSB.println(cmd);
        // TWIST MOTOR COMMAND
        if (cmd == 0) {
            SerialUSB.readBytes((char *) &received.linear, sizeof(float));
            SerialUSB.readBytes((char *) &received.rotation, sizeof(float));
-           SerialUSB.print("GOT TWIST ");
-           SerialUSB.println((int) received.linear);
-           SerialUSB.println((int) received.rotation);
+           // SerialUSB.print("GOT TWIST ");
+           // SerialUSB.println((int) received.linear);
+           // SerialUSB.println((int) received.rotation);
            setSparkVel((int) ((received.linear + received.rotation) * 100), (int) ((received.linear - received.rotation) * -100));
        }
        // TANK MOTOR COMMAND
        else if (cmd == 1) {
            SerialUSB.readBytes((char *) &received.linear, sizeof(float));
            SerialUSB.readBytes((char *) &received.rotation, sizeof(float));
-           SerialUSB.print("GOT TANK ");
-           SerialUSB.println((int) received.linear);
-           SerialUSB.println((int) received.rotation);
+           // SerialUSB.print("GOT TANK ");
+           // SerialUSB.println((int) received.linear);
+           // SerialUSB.println((int) received.rotation);
            setSparkVel((int) (received.linear * 100), (int) (-received.rotation * 100));
        }
        // ZED SERVO MOTOR COMMAND
        else if (cmd == 2) {
            SerialUSB.readBytes((char *) &received.zed_servo, sizeof(float));
-           SerialUSB.print("GOT SERVO ");
-           SerialUSB.println((int) received.zed_servo);
+           // SerialUSB.print("GOT SERVO ");
+           // SerialUSB.println((int) received.zed_servo);
            ZEDServo.write((int) received.zed_servo);
        }
    }
