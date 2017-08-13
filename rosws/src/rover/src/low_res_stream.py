9997e30bba4584f15b18e6f852bc277f9d33992f
Daniel Snider ツ <danielsnider12@gmail.com>
2017-08-13 18:25:37 -0400

    Update low_res_stream.py

--- rosws/src/rover/src/low_res_stream.py
+++ rosws/src/rover/src/low_res_stream.py
@@ -2,7 +2,6 @@
 import sys
 import rospy
 import cv2
-# import image_transport
 
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
@@ -10,10 +9,12 @@ from cv_bridge import CvBridge, CvBridgeError
 
 pub = rospy.Publisher('/low_res', Image, queue_size=1)
 def streamer(data):
+    # Reduce bandwith by converting to image to greyscale
     img = CvBridge().imgmsg_to_cv2(data, desired_encoding="mono8")
 
+    ## Make image smaller to reduce bandwith even more
     # resize img to at least 400 wide
-    scale_factor = 10.0/img.shape[1]
+    scale_factor = 400.0/img.shape[1]
     img = cv2.resize(img, (int(scale_factor*img.shape[1]), int(scale_factor*img.shape[0])), interpolation = cv2.INTER_CUBIC)
 
     img = CvBridge().cv2_to_imgmsg(img)
@@ -22,7 +23,6 @@ def streamer(data):
 if __name__ == '__main__':
   try:
     rospy.init_node('low_res_stream')
-    # TODO: BUFFERING NOT WORKING
     rospy.Subscriber('/usb_cam/image_raw', Image, streamer, queue_size=1)
     rospy.spin()
   except rospy.ROSInterruptException:
